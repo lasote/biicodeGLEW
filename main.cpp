5c1ae898b7728815444e7a8fc7fbfd6a32540acb
Dimitri Kourkoulis <developer@dimitros.be>
2014-10-30 01:26:13 +0100

    Works on Debian

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-#include "google/gtest/gtest.h"
-
 #include <iostream>
 #include "glew.h"
 
 using namespace std;
 
-TEST(GlewTest, ShowGlewVersion) {
-  	cout << "GLEW version: " << glewGetString(GLEW_VERSION) << endl;
-}
-
 int main(int argc, char **argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+  	cout << "GLEW version: " << glewGetString(GLEW_VERSION) << endl;
 }
